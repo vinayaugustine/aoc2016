7526b76c0e7c37da42e93c092d18da28b12de4df
Vinay Augustine <vinay.augustine@us.abb.com>
2016-12-02 11:37:49 -0500

    Add day 2 part 1

---
+++ 2/bathroom.fsx
@@ -0,0 +1,48 @@
+open System.IO
+
+type Move = Up='U' | Down='D' | Left='L' | Right='R'
+type Action = { move:Move; shouldPress:bool }
+type Position = {index:int*int; shouldPress:bool }
+
+let charToMove c = LanguagePrimitives.EnumOfValue<char, Move>(c)
+
+let executeMove (min:int*int) (max:int*int) (position:Position) (action:Action) =
+    let xmin,ymin = min
+    let xmax,ymax = max
+    let x,y = position.index
+    let x,y = match action.move with
+              | Move.Up    -> if y < ymax then x,y+1 else x,y
+              | Move.Down  -> if y > ymin then x,y-1 else x,y
+              | Move.Right -> if x < xmax then x+1,y else x,y
+              | Move.Left  -> if x > xmin then x-1,y else x,y
+              | _ -> failwithf "Invalid move %A" action.move
+    { index=x,y; shouldPress=action.shouldPress }
+
+let rec executeMoves (min:int*int) (max:int*int) start actionList =
+    match actionList with
+    | [] -> []
+    | head::tail ->
+        let nextPos = executeMove min max start head 
+        nextPos :: executeMoves min max nextPos tail 
+let lineToActions (line:string) =
+    let actions = [for i in 1 .. (line.Length-1) -> false] @ [true]
+    let moves = [for c in line -> charToMove c]
+    [for m,a in (Seq.zip moves actions) -> {move=m; shouldPress=a}]
+
+let readInput fileName =
+    seq { for l in File.ReadLines fileName -> l }
+    |> (Seq.map lineToActions)
+    |>  (Seq.reduce (@))
+
+let run fileName =
+    // TODO figure out Array2D
+    let keypad = [|[|7;8;9|];
+                   [|4;5;6|];
+                   [|1;2;3|]|]
+    let positions = executeMoves (0,0) (2,2) { index=1,1; shouldPress=false } (readInput fileName)
+    for x,y in [for p in positions do if p.shouldPress then yield p.index] do
+        // printfn "%i,%i" x y
+        printfn "%i" keypad.[y].[x]
+
+// run "test.txt"
+run "input.txt"
