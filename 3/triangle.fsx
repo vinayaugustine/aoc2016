4f085a9ba85388f4a982511c733f481cb39f1533
Vinay Augustine <vinay.augustine@us.abb.com>
2016-12-05 12:24:33 -0500

    Add day 3 part 1

---
+++ 3/triangle.fsx
@@ -0,0 +1,27 @@
+open System
+open System.IO
+
+let testTriangle l =
+    match l with
+    | [x;y;z] -> x + y > z
+    | _ -> failwith "the triangle does not have three sides"
+
+let testTriangles triangles =
+    triangles
+    |> Seq.map testTriangle
+
+let lineToList (line:string) =
+    line.Split [|' '|]
+    |> Array.toList
+    |> List.filter (fun v -> v.Length > 0)
+    |> List.map Int32.Parse
+    |> List.sort
+
+
+let readInput fileName =
+    seq { for l in File.ReadLines fileName -> l }
+    |> Seq.map lineToList
+
+readInput @"3\input.txt"
+|> Seq.filter testTriangle
+|> Seq.length
