2bfc900faef4ae11cfc6ecb01aad29cf4f3a2e03
Vinay Augustine <vinay.augustine@us.abb.com>
2016-12-07 09:57:51 -0500

    Add day 4 part 2

--- 4/security.fsx
+++ 4/security.fsx
@@ -3,9 +3,7 @@ open System.IO
 open System.Text.RegularExpressions
 
 type SecurityId = { Id:string; SectorId:int; Checksum:string }
-
 let idPattern = @"(?<id>([a-z]+-)+)(?<sectorId>\d+)\[(?<checkSum>[a-z]+)\]"
-
 let (|LineMatch|_|) pattern input =
     let m = Regex.Match(input, pattern) in
     if m.Success then
@@ -15,7 +13,6 @@ let (|LineMatch|_|) pattern input =
                Checksum=m.Groups.["checkSum"].Value
              }
         else None
-
 let parseLine pattern line =
     match line with
     | LineMatch pattern id -> id
@@ -33,15 +30,33 @@ let idIsValid (checkSumLength:int) (secId:SecurityId) =
     |> Array.ofSeq
     |> System.String = secId.Checksum
 
+let shift (count:int) (c:char)  = char (((int c) - 97 + count) % 26 + 97) 
+let decryptId (secId:SecurityId) =
+    let cipher = shift secId.SectorId
+    secId.Id.Split('-')
+    |> Seq.ofArray
+    |> Seq.map (fun word -> word |> Seq.map cipher |> Array.ofSeq |> System.String)
+    |> Array.ofSeq
+    |> String.concat " "
+
 let idIsValid5 = idIsValid 5
 let lines = [ "aaaaa-bbb-z-y-x-123[abxyz]";
               "a-b-c-d-e-f-g-h-987[abcde]";
               "not-a-real-room-404[oarel]";
               "totally-real-room-200[decoy]"]
 // let input = Seq.ofList lines
+// printfn "%s" (decryptId { Id="qzmt-zixmtkozy-ivhz"; SectorId=343; Checksum="abcde" })
+
 let input = seq { for l in File.ReadLines "input.txt" -> l }
 
-input |> Seq.map parseLineDefault
-      |> Seq.filter idIsValid5
-      |> Seq.sumBy (fun id -> id.SectorId)
-      |> printfn "sector sum = %i"
+// input |> Seq.map parseLineDefault
+//       |> Seq.filter idIsValid5
+//       |> Seq.sumBy (fun id -> id.SectorId)
+//       |> printfn "sector sum = %i"
+
+let decrypted = input |> Seq.map parseLineDefault
+                      |> Seq.filter idIsValid5
+                      |> Seq.map (fun id -> sprintf "%i %s" id.SectorId (decryptId id))
+                      |> Array.ofSeq
+                      |> String.concat Environment.NewLine
+printfn "%s" decrypted
